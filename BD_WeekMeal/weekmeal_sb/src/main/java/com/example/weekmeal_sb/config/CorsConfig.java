fa8c30a1fbb2838875456395eb5335d4441fe9e6
Javier Navarro Lázaro <jnl941@inlumine.ual.es>
2024-05-21 22:31:11 +0200

    Docker composes y CorsConfig arreglados (?), hace falta testearlos

--- BD_WeekMeal/weekmeal_sb/src/main/java/com/example/weekmeal_sb/config/CorsConfig.java
+++ BD_WeekMeal/weekmeal_sb/src/main/java/com/example/weekmeal_sb/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.example.weekmeal_sb;
+package com.example.weekmeal_sb.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
