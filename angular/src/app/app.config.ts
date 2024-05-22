3aaf8300785088ec773d74468f27409237ba9679
Javier Navarro Lázaro <jnl941@inlumine.ual.es>
2024-05-22 12:10:08 +0200

    Dockerfiles lanzan Springboot, Postgre y Angular

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -2,14 +2,14 @@
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-
+  
   providers: [provideRouter(routes), provideClientHydration()]
 };*/
 // app.config.ts
+import { provideClientHydration } from '@angular/platform-browser';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
