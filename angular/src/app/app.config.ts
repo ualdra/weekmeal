ecf7e6629f914a1b33436ec9a3b23330726e718e
Cristina Garcia Aguero <cga353@inlumine.ual.es>
2024-05-22 11:27:07 +0200

    Receta detalle avance

--- angular/src/app/app.config.ts
+++ angular/src/app/app.config.ts
@@ -1,19 +1,8 @@
-/*import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
-
-export const appConfig: ApplicationConfig = {
-
-  providers: [provideRouter(routes), provideClientHydration()]
-};*/
-// app.config.ts
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()]
