56b4cd1d53683360798780297f43b389010e6c27
Ayoub Fehri <afb356@inlumine.ual.es>
2024-06-02 15:57:22 +0200

    Modificacion en el pom y arreglo vista perfil-datos-personales

--- BD_WeekMeal/weekmeal_sb/src/main/java/com/example/weekmeal_sb/repository/ToleranciaRepository.java
+++ BD_WeekMeal/weekmeal_sb/src/main/java/com/example/weekmeal_sb/repository/ToleranciaRepository.java
@@ -3,7 +3,6 @@ import com.example.weekmeal_sb.entity.Tolerancia;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableJpaRepositories
